ac92461b96478d6b9fef5aa450d414c799e234cb
aamocca <aaroncafeina@hotmail.com>
2022-01-21 09:52:13 -0600

    "nuevas millas"

--- millas.js
+++ millas.js
@@ -4,4 +4,8 @@ console.log("a 10 la bolsita");
 console.log("a 10 la toronja");
 console.log("con sal y limon y chile");
 
+console.log("a 20 la bolsita");
+console.log("a 20 la toronja");
+console.log("con34 sal y limon y chile");
+
 console.log("Segundo cambio");
